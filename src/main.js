5925ddd63ea3a30b475a4dbe82e14a0151a47412
yingchaojie <yingchaojie@163.com>
2021-10-27 11:12:22 +0800

    fix echarts

--- src/main.js
+++ src/main.js
@@ -2,28 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {configKitInit} from "../lib/store/configkit";
-const app = createApp(App)
-configKitInit(import.meta.env)
-
-// plugins组件
-import {pluginEcharts} from "../lib/plugin/echarts";
-pluginEcharts(app)
-// 全局注册lib组件
+import {useRouter} from "../lib/router";
+import {routes} from "./router";
+import {routeBaseAfter, routeBaseBefore} from "../lib/router/intercepter";
 import {ComponentName} from "../lib/components";
 import kitEmpty from '../lib/components/kit-empty.vue'
 import kitModal from '../lib/components/kit-modal.vue'
 import kitTip from '../lib/components/kit-tip.vue'
 import kitTable from '../lib/components/kit-table.vue'
+
+const app = createApp(App)
+configKitInit(import.meta.env)
+// plugins组件
+import {pluginEcharts} from "../lib/plugin/echarts";
+pluginEcharts(app)
+// 全局注册lib组件
 app.component(ComponentName.Empty, kitEmpty)
 app.component(ComponentName.Modal, kitModal)
 app.component(ComponentName.Tip, kitTip)
 app.component(ComponentName.Table, kitTable)
 
 // router
-import {useRouter} from "../lib/router";
-import {routes} from "./router";
-import {routeBaseAfter, routeBaseBefore} from "../lib/router/intercepter";
-
 const router = useRouter(app, routes)
 router.beforeEach((to,from,next)=>{
   routeBaseBefore(to,from,next)
--- src/views/page1/page1.vue
+++ src/views/page1/page1.vue
@@ -2,7 +2,7 @@
   <div v-loading="loading">
     <kit-tip>abc</kit-tip>
     <kit-empty @click="modal.visible = true">test<span style="color: red">abc</span></kit-empty>
-    <kit-empty>test<span style="color: rebeccapurple">abc</span></kit-empty>
+    <v-chart style="width: 300px;height: 200px" :option="option1"></v-chart>
     <kit-modal :modal="modal" :confirm="confirm2">
       <template #title>abc</template>
       tst
@@ -14,8 +14,10 @@
   // import {errorMessage} from "../../../lib/service/message";
   import {sleep} from "../../../lib/utils/logic";
   import {useLoading} from "../../../lib/service/loading";
+  import {chartConfig} from "../../../lib/utils/echarts-helper";
 
   const loading = ref(false)
+  const option1 = ref()
   const modal = ref({
     visible: false
   })
@@ -28,5 +30,21 @@
 
   onMounted(useLoading(loading,async ()=>{
     await sleep(2000)
+    option1.value=chartConfig({
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    });
+    console.log(option1.value)
   }))
 </script>
